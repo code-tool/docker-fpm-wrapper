61572763d162ff4f72203fdf914b3aacfe0c9e07
miktwon <miktwon@syneforge.com>
2017-08-08 15:28:22 +0300

    prometheus

---
+++ fpmConfig/fpmConfig_test.go
@@ -0,0 +1,17 @@
+package fpmConfig
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestParse(t *testing.T) {
+	c, err := Parse("testData/php-fpm.conf")
+	assert.Nil(t, err)
+	assert.Equal(t, "testData/php-fpm.d/*.conf", c.Include)
+
+	assert.Equal(t, "www", c.Pools[0].Name)
+	assert.Equal(t, "/run/php-fpm/php-fpm.sock", c.Pools[0].Listen)
+	assert.Equal(t, "/status", c.Pools[0].StatusPath)
+}
