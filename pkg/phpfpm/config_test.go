c1d82e856eafc94da247b89a145f97b0c5963bd1
Yurii Berest <djua.com@gmail.com>
2018-06-23 23:34:38 +0300

    Refactored config parser tests

--- pkg/phpfpm/config_test.go
+++ pkg/phpfpm/config_test.go
@@ -3,13 +3,21 @@ package phpfpm
 import (
 	"testing"
 
+	"github.com/otiai10/copy"
 	"github.com/stretchr/testify/assert"
+	"os"
 )
 
 func TestParse(t *testing.T) {
-	c, err := ParseConfig("testData/php-fpm.conf")
+	dir := "/tmp/fpm-test"
+	assert.NoError(t, os.Mkdir(dir, 0777))
+	copy.Copy("testdata", dir)
+
+	defer os.RemoveAll(dir)
+
+	c, err := ParseConfig(dir + "/php-fpm.conf")
 	assert.Nil(t, err)
-	assert.Equal(t, "testData/php-fpm.d/*.conf", c.Include)
+	assert.Equal(t, dir + "/php-fpm.d/*.conf", c.Include)
 
 	assert.Equal(t, "www", c.Pools[0].Name)
 	assert.Equal(t, "/run/php-fpm/php-fpm.sock", c.Pools[0].Listen)
